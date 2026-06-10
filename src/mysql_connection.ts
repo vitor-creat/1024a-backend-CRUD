d5573d69675ff4c2c7e965d7274205c734e10c0e
gustavosilva-hub <gustavo.silva@ifms.edu.br>
2026-06-09 21:18:55 -0400

    Remove password from MySQL connection configuration

--- src/mysql_connection.ts
+++ src/mysql_connection.ts
@@ -3,7 +3,6 @@ import mysql from 'mysql2/promise';
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root',
     database: 'luademel',
 });
 export default connection
